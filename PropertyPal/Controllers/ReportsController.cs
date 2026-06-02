79ac834f7f4b48f5fb3c4a534ef49c259d292878
komail112 <202300533@student.polytechnic.bh>
2026-06-02 13:41:06 +0300

    Add maintenance backlog report page

---
+++ PropertyPal/Controllers/ReportsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PropertyPal.Mvc.ViewModels;
+
+namespace PropertyPal.Mvc.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult MaintenanceBacklog()
+        {
+            var backlog = new List<MaintenanceBacklogViewModel>
+            {
+                new MaintenanceBacklogViewModel
+                {
+                    Id = 1,
+                    PropertyName = "Seef Apartment 12",
+                    TenantName = "Ali Hassan",
+                    IssueTitle = "Air conditioner not cooling",
+                    Priority = "High",
+                    Status = "Open",
+                    RequestedDate = DateTime.Today.AddDays(-5)
+                },
+                new MaintenanceBacklogViewModel
+                {
+                    Id = 2,
+                    PropertyName = "Juffair Villa 4",
+                    TenantName = "Sara Ahmed",
+                    IssueTitle = "Water leakage in kitchen",
+                    Priority = "Medium",
+                    Status = "In Progress",
+                    RequestedDate = DateTime.Today.AddDays(-3)
+                }
+            };
+
+            return View(backlog);
+        }
+    }
+}
